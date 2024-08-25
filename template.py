a65450865f2f6b0ee6d03a4b465b9ea84ee5c579
Manish-Kumar24 <manishkumar202209@gmail.com>
2024-08-25 11:48:00 +0530

    requirements added

--- template.py
+++ template.py
@@ -26,7 +26,7 @@ list_of_files = [
 ]
 
 for filepath in list_of_files:
-    filepath = Path(filepath)
+    filepath = Path(filepath)      #type:ignore
     filedir, filename = os.path.split(filepath)
     if filedir !="":
         os.makedirs(filedir, exist_ok=True)
